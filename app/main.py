68c1f5af633fd40c610f3759db58ef5d3f1d5dbc
Преподаватель Топ <prep@step.lan>
2024-03-20 21:27:52 +0700

    main

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import json
 from fastapi.responses import HTMLResponse
-
+import uvicorn
 from pydantic import BaseModel
 
 app = FastAPI() # <- создаем экземпляр класса
@@ -14,13 +14,22 @@ URLS = [
     {"tasks_post" : "127.0.0.1:8000/tasks/"}
 ]
 
-def database(name_db, mode, data=None):
-    if mode == 'r':
-        with open(name_db, mode, encoding='utf-8') as db:
+class DatabaseJson:
+    def __init__(self, name_db) -> None:
+        self.__name_db = name_db
+
+    def name_db(self):
+        return self.__name_db
+
+    def read(self) -> dict:
+        with open(self.__name_db, 'r', encoding='utf-8') as db:
             return json.load(db)
-    elif mode == 'w':
-        with open(name_db, mode, encoding='utf-8') as db:
-            json.dump(data, db)
+    
+    def write(self, data) -> None:
+        with open(self.__name_db, 'w', encoding='utf-8') as db:
+            json.dump(data, db, ensure_ascii=False)
+
+database = DatabaseJson('database.json')
 
 
 class Task(BaseModel):
@@ -40,9 +49,15 @@ def create_task(request:Request, task:Task):
 
 @app.get('/tasks/')
 def get_tasks(request:Request):
-    tasks = database('database.json', 'r')
-    tasks['url'] = 'http://127.0.0.1:8000/tasks/'
+    tasks = database.read()
     return templates.TemplateResponse(request=request, name='tasks.html', context=tasks)
+
+
+
+if __name__ == '__main__':
+    print('Starting server')
+    uvicorn.run('main:app', port=8000, reload=True)
+    print('Server stopped')
 
 # ls - комадна показывающая в консоле папки и файлы
 # cd app - что бы перейти в директорию app
